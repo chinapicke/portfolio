c9b7e567563dc181c40f1619f9f0d54baf6e77d7
China Pickering <chinapickering97@gmail.com>
2023-12-27 16:12:47 +0000

    changed about layout

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -51,50 +51,80 @@ const About = () => {
       </div>
 
       <div className='middleSectionAboutPg'>
-        <div className='middleSectionItem'>
-          <h2 className='aboutSubheadings'>/01 STORY</h2>
-          <h3>A little about me.</h3>
-        </div>
-        <div className='middleSectionItem' id='aboutText'>
-          <p>Transitioning from mending the body to crafting digital landscapes, I bring a unique perspective to front-end development, where my background as a physiotherapist fuels my passion for
-            designing user interfaces which are captivating and the user' friendly.</p>
-          <p >Working in healthcare during a pandemic increased my curiosity for technology and the capabilities of coding. It wasn't until I took a year travelling the world that I fully dived in coding and instantly fell in love with it.</p>
-          <p> Despite travelling in Asia and the time difference, I completed a 16-week Bootcamp in the UK and refined my skills to help further my understanding of HTML, CSS, Javascript, React and Node.js.
-          </p>
-        </div>
-        <div className='banner bg-black'>
-          <div className='rightToLeftText'>
-            <p>FrontEnd Developer</p>
-            <p>User Experience</p>
-            <p>FrontEnd Developer</p>
+        {/* <div className='sectionGrid'>
+          <div className='ml-10'>
+            <h2 className='aboutSubheadings'>/01 STORY</h2>
+            <h3>A little about me.</h3>
           </div>
-          <div className='leftToRightText '>
-            <p>Responsive Design</p>
-            <p>Test-Driven Development</p>
-            <p>User Research</p>
-          </div>
-        </div>
-        <div className='middleSectionItem pt-5'>
-          <p>After my bootcamp, I continued to travelled until I returned back to the UK, where I completed a 12-week internship at a healthtech company.
+          <div className='middleSectionItem mb-2' id='aboutText'>
+            <p>Transitioning from mending the body to crafting digital landscapes, I bring a unique perspective to front-end development, where my background as a physiotherapist fuels my passion for
+              designing user interfaces which are captivating and the user' friendly.</p>
+            <p >Working in healthcare during a pandemic increased my curiosity for technology and the capabilities of coding. It wasn't until I took a year travelling the world that I fully dived in coding and instantly fell in love with it.</p>
+            <p> Despite travelling in Asia and the time difference, I completed a 16-week Bootcamp in the UK and refined my skills to help further my understanding of HTML, CSS, Javascript, React and Node.js.
+            </p>
+            <p>After my bootcamp, I continued to travelled until I returned back to the UK, where I completed a 12-week internship at a healthtech company.
             I was able to leverage my healthcare background to collaborate and develop a worldwide used contracting website that monitored the effectiveness of health technologies and services.
           </p>
           <p>I'm truly excited to see what the future holds and eager to find my place in a collaborative team where I can continue to do what I love!</p>
+          </div>
+        </div> */}
+        <div id='softSkillsSection' className='middleSectionItem sectionGrid'>
+        <div>
+        <h2 className='aboutSubheadings'>/01 STORY</h2>
+        <h3>A little about me.</h3>
+        </div>
+        <div>
+        <p>Transitioning from mending the body to crafting digital landscapes, I bring a unique perspective to front-end development, where my background as a physiotherapist fuels my passion for
+              designing user interfaces which are captivating and the user' friendly.</p>
+            <p >Working in healthcare during a pandemic increased my curiosity for technology and the capabilities of coding. It wasn't until I took a year travelling the world that I fully dived in coding and instantly fell in love with it.</p>
+            <p> Despite travelling in Asia and the time difference, I completed a 16-week Bootcamp in the UK and refined my skills to help further my understanding of HTML, CSS, Javascript, React and Node.js.
+            </p>
+            <p>After my bootcamp, I continued to travelled until I returned back to the UK, where I completed a 12-week internship at a healthtech company.
+            I was able to leverage my healthcare background to collaborate and develop a worldwide used contracting website that monitored the effectiveness of health technologies and services.
+          </p>
+          <p>I'm truly excited to see what the future holds and eager to find my place in a collaborative team where I can continue to do what I love!</p>
+          
+          </div>
+      </div>
+                <div className='banner bg-black'>
+          <div className='rightToLeftText'>
+            <p>FrontEnd Developer</p>
+            <p>.</p>
+            <p>User Experience</p>
+            <p>.</p>
+            <p>FrontEnd Developer</p>
+            <p>.</p>
+          </div>
+          <div className='leftToRightText '>
+            <p>Responsive Design </p>
+            <p>.</p>
+            <p>Test-Driven Development</p>
+            <p>.</p>
+            <p>User Research</p>
+            <p>.</p>
+          </div>
         </div>
       </div>
 
       <div id='waterfallImgDiv' className='middleSectionItem'>
         <img src={Bali} alt='Waterfall' className='waterfallAbout'></img>
       </div>
-      <div id='softSkillsSection' className='middleSectionItem'>
+      <div id='softSkillsSection' className='middleSectionItem sectionGrid'>
+        <div>
         <h2 className='aboutSubheadings'>/02 THE PROCESS</h2>
         <h3>How I work.</h3>
+        </div>
+        <div>
         <p> Through my experience I'm able to work under high levels of pressure and take initative with my work. I bring strong communication and collaboration abilities, honed through working closely with cross-functional teams in healthcare settings
           Futhermore, I am confident in working indepednently and collaboratively, whilst being able to problem-solve through any challenges I face.</p>
+          </div>
       </div>
 
-      <div className='middleSectionItem pt-5'>
+      <div className='middleSectionItem pt-5 sectionGrid'>
+        <div>
         <h2 className='aboutSubheadings'>/03 SKILLS</h2>
         <h3>What I use.</h3>
+        </div>
         <div className='skillsDiv'>
           <div className='skillsList'>
             <img src={HTML} alt='HTML skill' className='skillIcon' />
@@ -111,7 +141,36 @@ const About = () => {
         </div>
       </div>
 
-      <div className='middleSectionItem pt-5'>
+      <div id='softSkillsSection' className='middleSectionItem sectionGrid'>
+        <div>
+        <h2 className='aboutSubheadings'>/04 MORE INFO</h2>
+        <h3>Find out more about me.</h3>
+        </div>
+        <div>
+        <h3 className='softSkillH3'>Past Experience.</h3>
+        <div className='contactLinks'>
+          <Link to={CV}
+          download="China_Pickering_CV"
+          target="_blank"
+          rel="noreferrer"
+          // className='cvLink'
+        >CV</Link>
+        </div>
+        <h3 className='softSkillH3'>Social.</h3>
+        <div className='contactLinks'>
+          <Link>LinkedIn</Link>
+          <span className='px-2'>/</span>
+          <Link>Github</Link>
+        </div>
+        <h3 className='softSkillH3'>Email.</h3>
+        <div className='contactLinks'>
+          <Link>chinapickering97@gmail.com</Link>
+        </div>
+
+        </div>
+      </div>
+
+      {/* <div className='middleSectionItem pt-5'>
         <h2 className='aboutSubheadings'>/04 MORE INFO</h2>
         <h3>Find out more about me.</h3>
         <Link to={CV}
@@ -135,7 +194,7 @@ const About = () => {
         <div className='contactLinks'>
           <Link>chinapickering97@gmail.com</Link>
         </div>
-      </div>
+      </div> */}
     </motion.div>
   )
 }
