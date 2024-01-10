cef9d7c706b7c54eaa400d97e559fbf2667e7ad6
China Pickering <chinapickering97@gmail.com>
2024-01-10 16:28:50 +0000

    mobile view adjustments for cards

--- src/Components/OneProjectCardMobile.js
+++ src/Components/OneProjectCardMobile.js
@@ -7,30 +7,39 @@ function OneProjectCardMobile({ project }) {
   return (
     <div>
       <div className="cardImage" >
-        <img className='appImage' src={project.image} alt='App preview' />
+        <Link to={project.deployedURL}>
+          <img className='appImage' src={project.image} alt='App preview' />
+        </Link>
       </div>
       <div className="cardText">
         <h1 className="projectName">{project.projectname}</h1>
         <h2 className="projectShortDescription">{project.quickDescription}</h2>
-
-        <div
-          className="linksGrid grid grid-cols-2 "           >
-          <div>
-            <Link to={project.deployedURL}>
-              <button className="workLinks">view project</button>
-            </Link>
-            <span className='circleShadow' />
+        <p className="mb-2 text-center">{project.description}</p>
+          <div
+            className="linksGrid" >
+            <div>
+            <div className='flex flex-row justify-center items-center'>
+            {/* <Link to={project.deployedURL}>
+                            <button className="workLinks">view project</button>
+                        </Link>
+                        <span className='circleShadow' /> */}
+            {
+              project.components.map((item) => {
+                return (
+                  <p className='mx-2'>{item}</p>
+                )
+              })
+            }
           </div>
-          <div>
-            <Link to={project.githubUrl}>
-              <FaGithub className="workLinks" />
-            </Link>
+              <Link to={project.githubUrl} className="flex justify-center">
+                <FaGithub className="workLinks" />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-  )
+      )
 }
 
-export default OneProjectCardMobile
+      export default OneProjectCardMobile
